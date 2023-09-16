78b64fd0ab7744f01a905450fcecd602eb9fe3f5
Tiana Lopez <tianadavin@gmail.com>
2023-09-16 15:21:23 -0700

    Adds function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin(array) {
+  let obj = array.find((item) =>
+    item.result === "W"
+  )
+  if (obj === undefined) {
+    return undefined
+  }
+  else {
+    return obj.year
+  }
+
+}
+superbowlWin([
+  { year: "2015", result: "O" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+])
